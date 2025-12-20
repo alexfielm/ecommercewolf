c7a73aa4cdd07a6ef35b56c7bd66b4d3543cf69c
alexfielm <afielmachado@icloud.com>
2025-12-20 19:27:48 -0300

    arreglo de rutas y nuevos comps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,18 @@
+import WolfWidget from '../components/WolfWidget'
+import NavList from './NavList'
+import Newsletter from './Newsletter'
+import Socialimg from './Socialimg'
+
 function Footer() {
     return (
-        <></>
+        <div className="footerContainer">
+            <div className="footerImg">
+                <WolfWidget/>
+                <Socialimg/>
+            </div>
+            <NavList/>
+            <Newsletter/>
+        </div>
     )
 }
 
